47842280505f992f42ca295cd2d4279f09756db2
qiang <guyan0319@gmail.com>
2018-12-01 17:44:44 +0800

    cookie

--- example/main.go
+++ example/main.go
@@ -7,15 +7,29 @@ import (
 )
 
 func sayHelloHandler(w http.ResponseWriter, r *http.Request) {
-
+	SetCookie(w, r)
+	cookie, err := r.Cookie("name")
+	if err == nil {
+		fmt.Println(cookie.Value)
+		fmt.Println(cookie.Domain)
+		fmt.Println(cookie.Expires)
+	}
 	//fmt.Fprintf(w, "Hello world!\n") //这个写入到w的是输出到客户端的
 }
 
 func main() {
-	fmt.Println(32 << 20)
-	i := 32 << 20
-	i = i / 1024 / 1024
-	fmt.Println(i)
 	http.HandleFunc("/", sayHelloHandler) //	设置访问路由
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func SetCookie(w http.ResponseWriter, r *http.Request) {
+	ck := &http.Cookie{
+		Name:   "name",
+		Value:  "hello",
+		Path:   "/",
+		Domain: "localhost",
+		MaxAge: 120,
+	}
+	w.Header().Set("set-cookie", ck.String()) //设置cookie  value 值可以包含空格
+	//http.SetCookie(w, ck)  //设置cookie  value 值可以不能包含空格
 }
