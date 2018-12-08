0b2750dbd2f2ef85b45394cf28c1feaae1d4d1ff
qiang <guyan0319@gmail.com>
2018-12-08 16:26:27 +0800

    reflect

--- example/main.go
+++ example/main.go
@@ -10,21 +10,26 @@ type User struct {
 	Name string
 	Age  int
 }
-type Manager struct {
-	User  //反射会将匿名字段作为一个独立字段来处理
-	Title string
-}
 
-func (u User) Login() {
-	fmt.Println("login")
+func (u User) Login(name string) {
+	fmt.Println("login" + name)
+}
+func (u User) LoginOut(name, name1 string) {
+	fmt.Println("loginout" + name)
+	fmt.Println("loginout" + name1)
 }
 
 func main() {
-	m := Manager{User: User{1, "Jack", 12}, Title: "123"}
-	t := reflect.TypeOf(m)
-	fmt.Printf("%#v\n", t.Field(0))                   //#号会将reflect的struct的详情页打印出来，可以看出来这是一个匿名字段
-	fmt.Printf("%#v \n", t.FieldByIndex([]int{0, 0})) //此时 我们就可以将User当中的ID取出来,这里面需要传进方法中的是一个int类型的slice，User相对于manager索引是0，id相对于User索引也是0
-	fmt.Printf("%v \n", t.FieldByIndex([]int{0, 1}))
-	v := reflect.ValueOf(m)
-	fmt.Printf("%#v\n", v.Field(0))
+	user := User{Id: 1, Name: "jerry", Age: 29}
+	val := reflect.ValueOf(&user) //获取Value类型，也可以使用reflect.ValueOf(&user).Elem()
+
+	params := make([]reflect.Value, 1)
+	params[0] = reflect.ValueOf("herry")
+	val.MethodByName("Login").Call(params) //通过名称调用方法
+	paramstwo := make([]reflect.Value, 2)
+	paramstwo[0] = reflect.ValueOf("herry")
+	paramstwo[1] = reflect.ValueOf("jack")
+	fmt.Println(params)
+	val.Method(1).Call(paramstwo) //通过方法索引调用，paramstwo 含有两个参数
+
 }
