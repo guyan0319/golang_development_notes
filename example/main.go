1d6ce918d4bde86382c99478a7102f042c6a076c
qiang <guyan0319@gmail.com>
2018-12-12 09:43:29 +0800

    修改

--- example/main.go
+++ example/main.go
@@ -1,12 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	p := new([2]int)
-	p[0] = 22
-	b := make([]int, 10, 50) //第一个参数是类型，第二个参数是分配的空间，第三个参数是预留分配空间
-	a := b[:cap(b)]
-	fmt.Println(a)
-
+	var a [1]int
+	c := a[:]
+	fmt.Println(c)
 }
