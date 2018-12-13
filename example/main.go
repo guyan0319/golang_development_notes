f33686089fb77f8377daf145932d3f8d8eae461f
qiang <guyan0319@gmail.com>
2018-12-13 10:31:52 +0800

    delve

--- example/main.go
+++ example/main.go
@@ -1,12 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	//_ "net/http/pprof"
+)
+
+func sayHelloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("path", r.URL.Path)
+	fmt.Println("scheme", r.URL.Scheme)
+	fmt.Fprintf(w, "Hello world!\n") //这个写入到w的是输出到客户端的
+}
 
 func main() {
-	args := []int{1, 2, 3, 4}
-	nums := make([]int, 5)
-	for _, e := range args {
-		nums[e] = e
-	}
-	fmt.Println(nums)
+	http.HandleFunc("/", sayHelloHandler) //	设置访问路由
+	//http.HandleFunc("/debug/pprof/", pprof.Index)
+	//http.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
+	//http.HandleFunc("/debug/pprof/profile", pprof.Profile)
+	//http.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
+	//http.HandleFunc("/debug/pprof/trace", pprof.Trace)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
