2edfc1970400446a4ad533413679ad7586afb7ca
qiang <guyan0319@gmail.com>
2018-12-14 19:04:19 +0800

    log

--- example/main.go
+++ example/main.go
@@ -1,31 +1,20 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"net/http"
-	_ "net/http/pprof"
-	"time"
+	"os"
 )
 
-func sayHelloHandler(w http.ResponseWriter, r *http.Request) {
-	hellowold(10000)
-	fmt.Println("path", r.URL.Path)
-	fmt.Println("scheme", r.URL.Scheme)
-	fmt.Fprintf(w, "Hello world!\n") //这个写入到w的是输出到客户端的
-}
-
 func main() {
-	http.HandleFunc("/", sayHelloHandler) //	设置访问路由
-	log.Fatal(http.ListenAndServe(":8080", nil))
-}
-
-func hellowold(times int) {
-	time.Sleep(time.Second)
-	var counter int
-	for i := 0; i < times; i++ {
-		for j := 0; j < times; j++ {
-			counter++
-		}
+	// 按照所需读写权限创建文件
+	f, err := os.OpenFile("filename.log", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+	if err != nil {
+		log.Fatal(err)
 	}
+	// 完成后延迟关闭，而不是习惯!
+	//defer f.Close()
+	//设置日志输出到 f
+	log.SetOutput(f)
+	//测试用例
+	log.Println("check to make sure it works")
 }
