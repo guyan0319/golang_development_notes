3128d065a64a179b8e44f4083dbd43bc670a5be2
qiang <guyan0319@gmail.com>
2018-12-21 19:04:39 +0800

    stack

--- example/main.go
+++ example/main.go
@@ -1,23 +1,57 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unsafe"
+)
+
+type Example struct {
+	BoolValue  bool
+	IntValue   int16
+	FloatValue float32
+}
 
 func main() {
-	var m map[string]int
-	m = map[string]int{"one": 1, "two": 2}
-	n := m
-	fmt.Printf("%p\n", &m) //0xc000074018
-	fmt.Printf("%p\n", &n) //0xc000074020
-	fmt.Println(m)         // map[two:2 one:1]
-	fmt.Println(n)         //map[one:1 two:2]
-	changeMap(&m)
-	fmt.Printf("%p\n", &m) //0xc000074018
-	fmt.Printf("%p\n", &n) //0xc000074020
-	fmt.Println(m)         //map[one:1 two:2 three:3]
-	fmt.Println(n)         //map[two:2 three:3 one:1]
-}
-func changeMap(m *map[string]int) {
-	//m["three"] = 3 //这种方式会报错 invalid operation: m["three"] (type *map[string]int does not support indexing)
-	(*m)["three"] = 3                    //正确
-	fmt.Printf("changeMap func %p\n", m) //changeMap func 0x0
+	example := &Example{
+		BoolValue:  true,
+		IntValue:   10,
+		FloatValue: 3.141592,
+	}
+
+	exampleNext := &Example{
+		BoolValue:  true,
+		IntValue:   10,
+		FloatValue: 3.141592,
+	}
+
+	alignmentBoundary := unsafe.Alignof(example)
+
+	sizeBool := unsafe.Sizeof(example.BoolValue)
+	offsetBool := unsafe.Offsetof(example.BoolValue)
+
+	sizeInt := unsafe.Sizeof(example.IntValue)
+	offsetInt := unsafe.Offsetof(example.IntValue)
+
+	sizeFloat := unsafe.Sizeof(example.FloatValue)
+	offsetFloat := unsafe.Offsetof(example.FloatValue)
+
+	sizeBoolNext := unsafe.Sizeof(exampleNext.BoolValue)
+	offsetBoolNext := unsafe.Offsetof(exampleNext.BoolValue)
+
+	fmt.Printf("example Size: %d\n", unsafe.Sizeof(example))
+
+	fmt.Printf("Alignment Boundary: %d\n", alignmentBoundary)
+
+	fmt.Printf("BoolValue = Size: %d Offset: %d Addr: %v\n",
+		sizeBool, offsetBool, &example.BoolValue)
+
+	fmt.Printf("IntValue = Size: %d Offset: %d Addr: %v\n",
+		sizeInt, offsetInt, &example.IntValue)
+
+	fmt.Printf("FloatValue = Size: %d Offset: %d Addr: %v\n",
+		sizeFloat, offsetFloat, &example.FloatValue)
+
+	fmt.Printf("Next = Size: %d Offset: %d Addr: %v\n",
+		sizeBoolNext, offsetBoolNext, &exampleNext.BoolValue)
+
 }
