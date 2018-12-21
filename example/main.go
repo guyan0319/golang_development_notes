ae96a2b4a6f24a7881d0cfd0d86938450fc92728
qiang <guyan0319@gmail.com>
2018-12-21 14:02:41 +0800

    pointers

--- example/main.go
+++ example/main.go
@@ -3,10 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	p := 5
-	change(&p)
-	fmt.Println("p=", p)
+	var m map[string]int
+	m = map[string]int{"one": 1, "two": 2}
+	n := m
+	fmt.Printf("%p\n", &m) //0xc000074018
+	fmt.Printf("%p\n", &n) //0xc000074020
+	fmt.Println(m)         // map[two:2 one:1]
+	fmt.Println(n)         //map[one:1 two:2]
+	changeMap(&m)
+	fmt.Printf("%p\n", &m) //0xc000074018
+	fmt.Printf("%p\n", &n) //0xc000074020
+	fmt.Println(m)         //map[one:1 two:2 three:3]
+	fmt.Println(n)         //map[two:2 three:3 one:1]
 }
-func change(p *int) {
-	*p = 0
+func changeMap(m *map[string]int) {
+	//m["three"] = 3 //这种方式会报错 invalid operation: m["three"] (type *map[string]int does not support indexing)
+	(*m)["three"] = 3                    //正确
+	fmt.Printf("changeMap func %p\n", m) //changeMap func 0x0
 }
