6d46daafc4733ccd8229878c46810f38bd140b16
guyan0319 <guyan0319@sohu.com>
2019-01-13 00:15:27 +0800

    test

--- example/main.go
+++ example/main.go
@@ -4,16 +4,37 @@ import (
 	"os"
 	"io/ioutil"
 	"fmt"
+	"bufio"
+	"io"
 )
 
 func main() {
 	filepath := "D:/gopath/src/golang_development_notes/example/log.txt"
-	//readone(filepath)
-	content ,err :=ioutil.ReadFile(filepath)
-	if err !=nil {
+	fi, err := os.Open(filepath)
+	if err != nil {
 		panic(err)
 	}
-	fmt.Println(string(content))
+	defer fi.Close()
+	r := bufio.NewReader(fi)
+
+	chunks := make([]byte, 1024, 10240)
+
+	buf := make([]byte, 1024)
+	for {
+		n, err := r.Read(buf)
+		if err != nil && err != io.EOF {
+			panic(err)
+		}
+
+		if 0 == n {
+			break
+		}
+		chunks = append(chunks, buf[:n])
+	}
+	fmt.Println(string(chunks))
+
+
+
 }
 func readone ( filepath string )  string{
 	file, err := os.Open(filepath)
