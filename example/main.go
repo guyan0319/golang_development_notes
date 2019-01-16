c5ca8e2df280436874839246156e8230950e23d4
qiang <guyan0319@gmail.com>
2019-01-16 09:36:59 +0800

    update

--- example/main.go
+++ example/main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-	"os"
-	"io/ioutil"
-	"fmt"
 	"bufio"
+	"fmt"
 	"io"
+	"io/ioutil"
+	"os"
 )
 
 func main() {
-	filepath := "D:/gopath/src/golang_development_notes/example/log.txt"
+	filepath := "D:/gopath/src/example/example/log.txt"
 	fi, err := os.Open(filepath)
 	if err != nil {
 		panic(err)
@@ -17,26 +17,24 @@ func main() {
 	defer fi.Close()
 	r := bufio.NewReader(fi)
 
-	chunks := make([]byte, 1024, 10240)
-
-	buf := make([]byte, 1024)
+	chunks := make([]byte, 0)
+	fmt.Println(chunks)
+	buf := make([]byte, 1024) //一次读取多少个字节
 	for {
 		n, err := r.Read(buf)
 		if err != nil && err != io.EOF {
 			panic(err)
 		}
-
+		fmt.Println(string(buf[:n]))
+		break
 		if 0 == n {
 			break
 		}
-		chunks = append(chunks, buf[:n])
+		chunks = append(chunks, buf[:n]...)
 	}
 	fmt.Println(string(chunks))
-
-
-
 }
-func readone ( filepath string )  string{
+func readone(filepath string) string {
 	file, err := os.Open(filepath)
 	if err != nil {
 		panic(err)
