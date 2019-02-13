1ae78ff3def6078b0b2e2b1ba757474e87586445
qiang <guyan0319@gmail.com>
2019-02-13 18:33:25 +0800

    协程

--- example/main.go
+++ example/main.go
@@ -1,26 +1,13 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"net/http"
-)
-
-func sayHelloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("path", r.URL.Path)
-	fmt.Println("scheme", r.URL.Scheme)
-	fmt.Fprintf(w, "Hello world!\n") //这个写入到w的是输出到客户端的
-	panic("ffff")
-}
-func sayHelloHandlerapi(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("path", r.URL.Path)
-	fmt.Println("scheme", r.URL.Scheme)
-	fmt.Fprintf(w, "Hello world!\n") //这个写入到w的是输出到客户端的
-	//panic("ffff")
-}
+import "fmt"
 
 func main() {
-	http.HandleFunc("/", sayHelloHandler)       //	设置访问路由
-	http.HandleFunc("/api", sayHelloHandlerapi) //	设置访问路由
-	log.Fatal(http.ListenAndServe(":8080", nil))
+
+	messages := make(chan string)
+
+	go func() { messages <- "ping" }()
+
+	msg := <-messages
+	fmt.Println(msg)
 }
