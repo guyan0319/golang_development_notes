b040721d967ab2d68a440c1c67cce7d11456d064
guyan0319 <guyan0319@sohu.com>
2019-03-25 23:23:17 +0800

    inject

--- example/public/goinject/inject.go
+++ example/public/goinject/inject.go
@@ -4,25 +4,22 @@ import (
 	"fmt"
 	"reflect"
 	"sync"
+
+	"regexp"
 )
 
 //    `inject:""`
-//    `inject:"sington"`
-//    `inject:"sington,demo"`
-//    `inject:"demo"`
-
-// An Object in the Graph.
+//    `inject:"inline"`
+//    `inject:"foo"`
+var injectTag = "inject"
 type Object struct {
 	Value        interface{}
-	Name         string // Optional
+	name         string
 	reflectType  reflect.Type
 	reflectValue reflect.Value
-	embedded     bool // If true, the Object is an embedded struct provided internally
-	private      bool // If true, the Value will not be used and will only be populated
-
+	embedded     bool
 }
 
-// The Graph of Objects.
 type Graph struct {
 	sync.Mutex
 	unnamed     []*Object
@@ -30,44 +27,46 @@ type Graph struct {
 	named       map[string]*Object
 }
 
-// Provide objects to the Graph. The Object documentation describes
-// the impact of various fields.
-func (g *Graph) Provide(objects ...*Object) error {
+func (g *Graph) Provider(objects ...*Object) error {
+	g.Lock()
+	defer g.Unlock()
+	if g.unnamedType == nil {
+		g.unnamedType = make(map[reflect.Type]bool)
+	}
 	for _, o := range objects {
 		o.reflectType = reflect.TypeOf(o.Value)
 		o.reflectValue = reflect.ValueOf(o.Value)
+		if !isStructPtr(o.reflectType) {
+			return fmt.Errorf(
+				"expected unnamed object value to be a pointer to a struct but got type %s "+
+					"with value %v",
+				o.reflectType,
+				o.Value,
+			)
+		}
+		if !o.embedded {
+			if g.unnamedType[o.reflectType] {
+				continue
+			}
+			g.unnamedType[o.reflectType] = true
+		}
+		g.unnamed = append(g.unnamed, o)
+	}
+	return nil
+}
+func (g *Graph) Ensure() error {
+	i := 0
+	for {
 
-		if o.Name == "" {
-			if !isStructPtr(o.reflectType) {
-				return fmt.Errorf(
-					"expected unnamed object value to be a pointer to a struct but got type %s "+
-						"with value %v",
-					o.reflectType,
-					o.Value,
-				)
-			}
-			if !o.private {
-				if g.unnamedType == nil {
-					g.unnamedType = make(map[reflect.Type]bool)
-				}
-				if g.unnamedType[o.reflectType] {
-					return fmt.Errorf(
-						"provided two unnamed instances of type *%s.%s",
-						o.reflectType.Elem().PkgPath(), o.reflectType.Elem().Name(),
-					)
-				}
-				g.unnamedType[o.reflectType] = true
-			}
-			g.unnamed = append(g.unnamed, o)
+		if i == len(g.unnamed) {
+			break
+		}
 
-		} else {
-			if g.named == nil {
-				g.named = make(map[string]*Object)
-			}
-			if g.named[o.Name] != nil {
-				return fmt.Errorf("provided two instances named %s", o.Name)
-			}
-			g.named[o.Name] = o
+		o := g.unnamed[i]
+		i++
+
+		if err := g.ensuresub(o); err != nil {
+			return err
 		}
 
 	}
@@ -76,3 +75,74 @@ func (g *Graph) Provide(objects ...*Object) error {
 func isStructPtr(t reflect.Type) bool {
 	return t.Kind() == reflect.Ptr && t.Elem().Kind() == reflect.Struct
 }
+
+func (g *Graph) ensuresub(o *Object) error {
+	if !isStructPtr(o.reflectType) {
+		return nil
+	}
+	//Loop:
+	for i := 0; i < o.reflectValue.Elem().NumField(); i++ {
+		field := o.reflectValue.Elem().Field(i)
+		fieldType := field.Type()
+		fieldTag := o.reflectType.Elem().Field(i).Tag
+		tag := matchTag(fieldTag)
+		fmt.Println(fieldTag.Get("inject"))
+		//fieldTag := o.reflectType.Elem().Field(i).Tag.Get("inject")
+
+		fmt.Println(fieldType,fieldTag,tag)
+		//fieldName := o.reflectType.Elem().Field(i).Name
+		//tag, err := parseTag(string(fieldTag))
+		//if err != nil {
+		//	return fmt.Errorf(
+		//		"unexpected tag format `%s` for field %s in type %s",
+		//		string(fieldTag),
+		//		o.reflectType.Elem().Field(i).Name,
+		//		o.reflectType,
+		//	)
+		//}
+
+	}
+	return nil
+}
+type tag struct {
+	Name    string
+	Inline  bool
+	Sington bool
+}
+func matchTag(t reflect.StructTag) (*tag){
+	if t =="" {
+		return nil
+	}
+	r := regexp.MustCompile("^"+injectTag+".*")
+	if r.MatchString(string(t)) {
+		name:= t.Get(injectTag)
+		switch name {
+		case "":
+			return &tag{Sington:true}
+		case "inline":
+			return &tag{Inline:true}
+		default:
+			return &tag{Name:name}
+		}
+	}
+    return nil
+}
+//func parseTag(t string) (*tag, error) {
+//found, value, err := structtag.Extract("inject", t)
+//if err != nil {
+//return nil, err
+//}
+//if !found {
+//return nil, nil
+//}
+//if value == "" {
+//return injectOnly, nil
+//}
+//if value == "inline" {
+//return injectInline, nil
+//}
+//if value == "private" {
+//return injectPrivate, nil
+//}
+//return &tag{Name: value}, nil
+//}
--- example/test.go
+++ example/test.go
@@ -2,75 +2,84 @@ package main
 
 import (
 	"fmt"
-	"reflect"
-	"unsafe"
+	"os"
+	"golang_development_notes/example/public/goinject"
 )
 
-type User struct {
-	Id   int
-	Name string
-	Age  int
+type DBEngine struct {
+	Key string
 }
 
-func (u User) Login() {
-	fmt.Println("login")
+func (d *DBEngine) Name() string {
+	return "DbName"
 }
 
+func NewDBEngine() *DBEngine {
+	return &DBEngine{}
+}
+
+type CacheEngine struct {
+	Key string
+	T   map[int]string
+}
+
+func (c *CacheEngine) Name() string {
+	return "CacheEngine"
+}
+
+func NewCacheEngine() *CacheEngine {
+	return &CacheEngine{}
+}
+
+type UserDB struct {
+	Db    *DBEngine    `inject:""`
+	Cache *CacheEngine `inject:""`
+}
+
+type ItemDB struct {
+	DBEngine `inject:"inline"`
+	Cache    *CacheEngine `inject:""`
+}
+
+type UserService struct {
+	Db *UserDB `inject:""`
+}
+
+type ItemService struct {
+	Db *ItemDB `inject:""`
+}
+
+type App struct {
+	User *UserService `inject:"fdsaf"`
+	Item *ItemService `inject:""`
+}
+
+func (a *App) Render() string {
+	return fmt.Sprintf(
+		"db name is %s ,cache name is %s.",
+		a.User.Db.Db.Name(),
+		a.Item.Db.Cache.Name(),
+	)
+}
 func main() {
-	u := User{1, "jerry", 23}
-	t := reflect.TypeOf(u) //反射出一个interface{}的类型,main.User
-	v := reflect.ValueOf(u)
-	for i := 0; i < t.NumField(); i++ { //通过索引来取得它的所有字段，这里通过t.NumField来获取它多拥有的字段数量，同时来决定循环的次数
-		f := t.Field(i)               //通过这个i作为它的索引，从0开始来取得它的字段
-		val := v.Field(i).Interface() //通过interface方法来取出这个字段所对应的值
-		fmt.Printf("%6s:%v =%v\n", f.Name, f.Type, val)
+
+	//db := NewDBEngine()
+	//cache := NewCacheEngine()
+	var g goinject.Graph
+	var app App
+	err := g.Provider(
+		&goinject.Object{Value: &app},
+		//&goinject.Object{Value: db},
+	)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
 	}
-	for i := 0; i < t.NumMethod(); i++ { //这里同样通过t.NumMethod来获取它拥有的方法的数量，来决定循环的次数
-		m := t.Method(i)
-		fmt.Printf("%6s:%v\n", m.Name, m.Type)
+
+	if err := g.Ensure(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
 	}
-
-	fmt.Println(t.Name())          //类型名 User
-	fmt.Println(t.Kind().String()) //Type类型表示的具体分类  struct
-	fmt.Println(t.PkgPath())       //反射对象所在的短包名 main
-	fmt.Println(t.String())        //包名.类型名  main.User
-	fmt.Println(t.Size())          //要保存一个该类型要多少个字节 32
-	fmt.Println(t.Align())         //返回当从内存中申请一个该类型值时，会对齐的字节数 8
-	fmt.Println(t.FieldAlign())    //返回当该类型作为结构体的字段时，会对齐的字节数 8
-
-	fmt.Println(t.AssignableTo(reflect.TypeOf(u)))  // 如果该类型的值可以直接赋值给u代表的类型，返回真 true
-	fmt.Println(t.ConvertibleTo(reflect.TypeOf(u))) // 如该类型的值可以转换为u代表的类型，返回真 true
-
-	fmt.Println(t.NumField())             // 返回struct类型的字段数（匿名字段算作一个字段），如非结构体类型将panic  3
-	fmt.Println(t.Field(0).Name)          // 返回struct类型的第i个字段的类型，如非结构体或者i不在[0, NumField())内将会panic  Id
-	fmt.Println(t.FieldByName("Age"))     // 返回该类型名为name的字段（会查找匿名字段及其子字段），布尔值说明是否找到，如非结构体将panic
-	fmt.Println(t.FieldByIndex([]int{0})) // 返回索引序列指定的嵌套字段的类型，等价于用索引中每个值链式调用本方法，如非结构体将会panic
-
-	//ValueOf
-	fmt.Println(v.IsValid()) //返回v是否持有值，如果v是value零值会返回假，此时v除了IsValid String Kind之外的方法都会导致panic
-	fmt.Println(v.Kind())    //返回v持有值的分类，如果v是value零值，返回值为invalid  struct
-	fmt.Println(v.Type())    //返回v持有值的类型Type表示  main.User
-
-	//结构体指针
-	//vv := &v
-	fmt.Println(v.Convert(reflect.TypeOf(u)).FieldByName("Name")) // //转换为其他类型的值,如果无法使用标准Go转换规则来转换，那么panic  jerry
-	pv := reflect.ValueOf(&u)
-	pp := pv.Elem() //返回持有的接口的值，或者指针的值，如果不是interface{}或指针会panic,实际上是从 *User到User
-	fmt.Println(pp)
-	fmt.Println(v.FieldByName("Name").CanSet())  //是否可以设置Name的值  false
-	fmt.Println(pp.FieldByName("Name").CanSet()) //是否可以设置Name的值  true
-	pp.FieldByName("Name").SetString("newname")  //设置Name的值
-	fmt.Println(u)                               //{1 newname 23}
-
-	fmt.Println(pp.FieldByName("Name").Interface()) //把Name当做interface{}值  newname
-	fmt.Println(pp.FieldByName("Name").String())    //返回v持有的值的字符串表示，如果v的值不是string也不会panic  newname
-	var x int64
-	fmt.Println(v.FieldByName("Age").OverflowInt(x)) //如果v持有值的类型不能溢出的表示x，会返回真，如果v的kind不是int int8-int64会panic false
-
-	//以下这种方式效率比较高些
-	sv := reflect.TypeOf(&u).Elem()
-	field, _ := sv.FieldByName("Name")
-	field1Ptr := uintptr(unsafe.Pointer(&u)) + field.Offset
-	*((*string)(unsafe.Pointer(field1Ptr))) = "Jerry"
-	fmt.Println(u)
+	//
+	//fmt.Println(app.Render())
 }
