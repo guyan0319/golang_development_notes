e036b682bca3fbbec45df92d438af7c89c5e0523
qiang <guyan0319@gmail.com>
2019-03-28 15:40:39 +0800

    closure

--- example/test.go
+++ example/test.go
@@ -1,19 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-	a := F()
-	a[0]()//3
-	a[1]()//3
-	a[2]()//3
-}
-func F() []func() {
-	b := make([]func(), 3, 3)
-	for i := 0; i < 3; i++ {
-		b[i] = func() {
-			fmt.Println(i)
-		}
+	c := 0
+	start := time.Now()
+	for i := 1; i < 2000000000; i++ {
+		//c = (func() int {
+		//	a := 1
+		//	b := 3
+		//	return a + b
+		//})()
+		c = F()
 	}
-	return b
+	fmt.Println(c)
+	t0 := time.Now()
+	fmt.Printf("Cost time %v\n", t0.Sub(start))
+}
+
+func F() int {
+	a := 1
+	b := 3
+	return a + b
 }
