abeb01ba27a905c785f712566a083db483a0540f
qiang <guyan0319@gmail.com>
2019-05-06 14:13:54 +0800

    test

--- example/public/common.go
+++ example/public/common.go
@@ -7,19 +7,25 @@ import (
 )
 
 func Examiner(t reflect.Type, depth int) {
-	fmt.Println(strings.Repeat("\t", depth), "Type is", t.Name(), "and kind is", t.Kind())
+	//fmt.Printf("%+v\n", t.Kind())
+	//fmt.Println(strings.Repeat("\t", depth), "Type is", t.Name(), "and kind is", t.Kind())
 	switch t.Kind() {
 	case reflect.Array, reflect.Chan, reflect.Map, reflect.Ptr, reflect.Slice:
-		fmt.Println(strings.Repeat("\t", depth+1), "Contained type:")
+		//fmt.Println(strings.Repeat("\t", depth+1), "Contained type:")
+		//fmt.Println(t)
+		//fmt.Println(t.Elem())
 		Examiner(t.Elem(), depth+1)
 	case reflect.Struct:
+		fmt.Printf("%v %v{\n", t.Name(), t.Kind())
 		for i := 0; i < t.NumField(); i++ {
 			f := t.Field(i)
-			fmt.Println(strings.Repeat("\t", depth+1), "Field", i+1, "name is", f.Name, "type is", f.Type.Name(), "and kind is", f.Type.Kind())
-			if f.Tag != "" {
-				fmt.Println(strings.Repeat("\t", depth+2), "Tag is", f.Tag)
-				fmt.Println(strings.Repeat("\t", depth+2), "tag1 is", f.Tag.Get("tag1"), "tag2 is", f.Tag.Get("tag2"))
-			}
+			fmt.Println(strings.Repeat("\t", depth+1), f.Name, f.Type.Name(), f)
+
+			//if f.Tag != "" {
+			//	fmt.Println(strings.Repeat("\t", depth+2), "Tag is", f.Tag)
+			//	fmt.Println(strings.Repeat("\t", depth+2), "tag1 is", f.Tag.Get("tag1"), "tag2 is", f.Tag.Get("tag2"))
+			//}
 		}
+		fmt.Println(strings.Repeat("\t", depth+1) + "}")
 	}
 }
---
+++ test3.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"example/example/public"
+	"fmt"
+	"reflect"
+)
+
+type Users struct {
+	Id   int
+	Name string
+	Age  int
+	Ext  Info
+}
+type Info struct {
+	Detail string
+}
+
+func (u Users) Login() {
+	fmt.Println("login")
+}
+
+func main() {
+
+	u := &Users{Id: 12}
+	t := reflect.TypeOf(u)
+	//ch := make(chan string)
+	//t := reflect.TypeOf(ch)
+	public.Examiner(t, 0)
+	//t := reflect.TypeOf(u).Elem() //反射出一个interface{}的类型,main.User
+	//v := reflect.ValueOf(u).Elem()
+	//for i := 0; i < t.NumField(); i++ { //通过索引来取得它的所有字段，这里通过t.NumField来获取它多拥有的字段数量，同时来决定循环的次数
+	//	f := t.Field(i)               //通过这个i作为它的索引，从0开始来取得它的字段
+	//	val := v.Field(i).Interface() //通过interface方法来取出这个字段所对应的值
+	//
+	//	fmt.Printf("%v:%+v =%v\n", f.Name, f.Type, val)
+	//}
+
+}
