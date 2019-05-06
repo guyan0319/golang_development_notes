2db1664223edd19608034f80368fb80fd4ca2229
qiang <guyan0319@gmail.com>
2019-05-06 17:21:00 +0800

    修改

--- example/public/common.go
+++ example/public/common.go
@@ -3,20 +3,35 @@ package public
 import (
 	"fmt"
 	"reflect"
+	"strings"
 )
 
 func Examiner(v reflect.Value, depth int) {
-	t := v.Type()
-	switch v.Kind() {
-	case reflect.Array, reflect.Chan, reflect.Map, reflect.Ptr, reflect.Slice:
-		Examiner(v.Elem(), 0)
-	case reflect.Struct:
-		for i := 0; i < v.NumField(); i++ {
-			f := v.Field(i)
-			fmt.Printf("%s %s : %v \n", t.Field(i).Name, f.Type(), f.Interface())
-		}
-	}
+	if v.CanInterface() {
+		t := v.Type()
+		switch v.Kind() {
+		case reflect.Ptr:
+			Examiner(v.Elem(), depth)
+		case reflect.Struct:
+			fmt.Printf(strings.Repeat("\t", depth)+"%v %v {\n", t.Name(), t.Kind())
+			for i := 0; i < v.NumField(); i++ {
+				f := v.Field(i)
+				if f.Kind() == reflect.Struct || f.Kind() == reflect.Ptr {
+					Examiner(f, depth+1)
+				} else {
+					if f.CanInterface() {
+						fmt.Printf(strings.Repeat("\t", depth+1)+"%s %s : %v \n", t.Field(i).Name, f.Type(), f.Interface())
+					} else {
 
+						fmt.Printf(strings.Repeat("\t", depth+1)+"%s %s : %v \n", t.Field(i).Name, f.Type(), f)
+					}
+				}
+			}
+			fmt.Println(strings.Repeat("\t", depth) + "}")
+		}
+	} else {
+		fmt.Printf(strings.Repeat("\t", depth)+"%+v\n", v)
+	}
 }
 
 // func Examiner(t reflect.Value, depth int) {
--- test3.go
+++ test3.go
@@ -7,13 +7,18 @@ import (
 )
 
 type Users struct {
-	Id   int
-	Name string
-	Age  int
-	Ext  Info
+	Id     int
+	Name   string
+	Age    int
+	Market map[int]string
+	Source *Sfrom
+	Ext    Info
 }
 type Info struct {
 	Detail string
+}
+type Sfrom struct {
+	Area string
 }
 
 func (u Users) Login() {
@@ -24,13 +29,21 @@ func main() {
 	//ch := make(chan string, 3)
 	//fmt.Println(ch)
 	//vv := runtime.Getchan()
-	u := &Users{Id: 12}
+	//v := reflect.ValueOf(vv)
+	//
+	//public.Examiner(v, 0)
+	//fmt.Printf("%+v", v)
+	m := map[int]string{1: "abc"}
+	s := &Sfrom{Area: "beijing"}
+	i := Info{Detail: "detail"}
+	u := &Users{Id: 12, Market: m, Ext: i, Source: s}
 	//t := reflect.TypeOf(u)
 	v := reflect.ValueOf(u)
+	public.Examiner(v, 0)
 	//fmt.Printf("%+v\n", v)
 
 	//t := reflect.TypeOf(ch)
-	public.Examiner(v, 0)
+	//public.Examiner(v, 0)
 	//public.Examiner(t, 0)
 	//t := reflect.TypeOf(u).Elem() //反射出一个interface{}的类型,main.User
 	//v := reflect.ValueOf(u).Elem()
