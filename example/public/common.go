2a5137374b4422ab99c7dc95bb484d8b9c2ff0da
qiang <guyan0319@gmail.com>
2019-05-07 08:59:09 +0800

    reflect

--- example/public/common.go
+++ example/public/common.go
@@ -33,29 +33,3 @@ func Explicit(v reflect.Value, depth int) {
 		fmt.Printf(strings.Repeat("\t", depth)+"%+v\n", v)
 	}
 }
-
-// func Examiner(t reflect.Value, depth int) {
-//	//fmt.Printf("%+v\n", t.Kind())
-//	//fmt.Println(strings.Repeat("\t", depth), "Type is", t.Name(), "and kind is", t.Kind())
-//	switch t.Kind() {
-//	case reflect.Array, reflect.Chan, reflect.Map, reflect.Ptr, reflect.Slice:
-//		//fmt.Println(strings.Repeat("\t", depth+1), "Contained type:")
-//		//fmt.Println(t)
-//		//fmt.Println(t.Elem())
-//		Examiner(t.Elem(), depth+1)
-//	case reflect.Struct:
-//		fmt.Printf("%v %v{\n", t.Name(), t.Kind())
-//		for i := 0; i < t.NumField(); i++ {
-//			f := t.Field(i)
-//			v := reflect.ValueOf(f)
-//			fmt.Println(v)
-//			fmt.Println(strings.Repeat("\t", depth+1), f.Name, f.Type.Name())
-//
-//			//if f.Tag != "" {
-//			//	fmt.Println(strings.Repeat("\t", depth+2), "Tag is", f.Tag)
-//			//	fmt.Println(strings.Repeat("\t", depth+2), "tag1 is", f.Tag.Get("tag1"), "tag2 is", f.Tag.Get("tag2"))
-//			//}
-//		}
-//		fmt.Println(strings.Repeat("\t", depth+1) + "}")
-//	}
-//}
