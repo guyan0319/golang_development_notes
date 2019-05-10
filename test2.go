27bebcb3a918efc1dde4b8feda1d9dd29866182d
qiang <guyan0319@gmail.com>
2019-05-10 14:23:00 +0800

    channel

--- test2.go
+++ test2.go
@@ -13,10 +13,27 @@ func goRoutineB(b chan int) {
 	val := <-b
 	fmt.Println("goRoutineB  received the data", val)
 }
+func goRoutineC(ch chan int, i int) {
+	ch <- i
+	ch <- 5
+}
 func main() {
 	ch := make(chan int, 3)
-	go goRoutineA(ch)
-	go goRoutineB(ch)
+	//ch := make(chan int)
+	//go goRoutineA(ch)
+	//go goRoutineB(ch)
+	//go goRoutineC(ch, 3)
+	//go goRoutineC(ch, 4)
 	ch <- 3
+	val := <-ch
+	fmt.Println("goRoutineB  received the data", val)
+	//ch <- 3
+	//ch <- 4
+	//ch <- 5
+	//ch <- 6
+	//ch <- 7
+	//ch <- 8
+	//ch <- 9
+
 	time.Sleep(time.Second * 1)
 }
