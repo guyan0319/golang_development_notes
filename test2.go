cc2b27fe16863de47a82329c0d929d62f69895b6
guyan0319 <guyan0319@sohu.com>
2019-05-10 23:12:12 +0800

    channel

--- test2.go
+++ test2.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-	"example/example/public"
 	"fmt"
-	"reflect"
-	"runtime"
 	"time"
 )
 
@@ -20,9 +17,10 @@ func goRoutineC(ch chan int, i int) {
 	ch <- i
 }
 func main() {
-	ch := make(chan int, 3)
+	ch := make(chan int,3)
+	fmt.Println(ch)
 	//ch := make(chan int)
-	go goRoutineA(ch)
+	//go goRoutineA(ch)
 	go goRoutineB(ch)
 	//go goRoutineC(ch, 3)
 	//go goRoutineC(ch, 4)
@@ -30,19 +28,19 @@ func main() {
 	//go goRoutineC(ch, 6)
 	//go goRoutineC(ch, 4)
 	ch <- 3
-	ch <- 4
+	//ch <- 4
 	//ch <- 5
 	//ch <- 6
 	//ch <- 7
 
 	//val := <-ch
 	//fmt.Println("goRoutineB  received the data", val)
-	vv := runtime.Getchan()
-	v := reflect.ValueOf(vv)
-	public.Explicit(v, 0)
-	ww := runtime.Getwaitq()
-	w := reflect.ValueOf(ww)
-	public.Explicit(w, 0)
+
+	//v := reflect.ValueOf(vv)
+	//public.Explicit(v, 0)
+	//ww := runtime.Getwaitq()
+	//w := reflect.ValueOf(ww)
+	//public.Explicit(w, 0)
 	//ch <- 3
 	//ch <- 4
 	//ch <- 5
