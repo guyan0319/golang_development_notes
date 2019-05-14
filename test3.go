764fe5721b6070725a2e318ca007872573a426af
qiang <guyan0319@gmail.com>
2019-05-14 18:43:22 +0800

    channel

--- test3.go
+++ test3.go
@@ -2,15 +2,44 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
 )
 
-func main() {
-	ch := make(chan int, 3)
-
-	ch <- 3
-	i, isClose := <-ch
-	if !isClose {
-
+func consumer(stop <-chan bool) {
+	for {
+		select {
+		case <-stop:
+			fmt.Println("exit sub goroutine")
+			return
+		default:
+			fmt.Println("running...")
+			time.Sleep(500 * time.Millisecond)
+		}
 	}
-	fmt.Println(<-ch)
+}
+func main() {
+	stop := make(chan bool)
+	var wg sync.WaitGroup
+	// Spawn example consumers
+	for i := 0; i < 3; i++ {
+		wg.Add(1)
+		go func(stop <-chan bool) {
+			defer wg.Done()
+			consumer(stop)
+		}(stop)
+	}
+	//waitForSignal()
+	close(stop)
+	fmt.Println("stopping all jobs!")
+	wg.Wait()
+}
+func waitForSignal() {
+	sigs := make(chan os.Signal)
+	signal.Notify(sigs, os.Interrupt)
+	signal.Notify(sigs, syscall.SIGTERM)
+	<-sigs
 }
--- test4.go
+++ test4.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+)
+
+// 监听全部信号
+func main() {
+	//合建chan
+	c := make(chan os.Signal)
+	//监听所有信号
+	signal.Notify(c)
+	//阻塞直到有信号传入
+	fmt.Println("启动")
+	s := <-c
+	fmt.Println("退出信号", s)
+}
