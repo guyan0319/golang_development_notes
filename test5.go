ba4ca4470ddcb08eb97ad57a1e64be196c9b7d57
qiang <guyan0319@gmail.com>
2019-05-16 20:07:28 +0800

    signal

--- test5.go
+++ test5.go
@@ -1,32 +1,11 @@
 package main
 
-import "fmt"
-import "os"
-import "os/signal"
-import "syscall"
+import (
+	"fmt"
+)
 
 func main() {
-
-	// 创建一个os.Signal channel
-	sigs := make(chan os.Signal, 1)
-	//创建一个bool channel
-	done := make(chan bool, 1)
-	//注册要接收的信号，syscall.SIGINT:接收ctrl+c ,syscall.SIGTERM:程序退出
-	//信号没有信号参数表示接收所有的信号
-	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
-
-	//此goroutine为执行阻塞接收信号。一旦有了它，它就会打印出来。
-	//然后通知程序可以完成。
-	go func() {
-		sig := <-sigs
-		fmt.Println(sig)
-		done <- true
-	}()
-	//不允许继续往sigs中存入内容
-	signal.Stop(sigs)
-	//程序将在此处等待，直到它预期信号（如Goroutine所示）
-	//在“done”上发送一个值，然后退出。
-	fmt.Println("awaiting signal")
-	<-done
-	fmt.Println("exiting")
+	println("first")
+	println("second")
+	fmt.Println("third")
 }
