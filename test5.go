9804a87d147a1d092e5e0e3dc18d6d03943a97be
qiang <guyan0319@gmail.com>
2019-05-22 14:23:11 +0800

    context

--- test5.go
+++ test5.go
@@ -1,60 +1,22 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"time"
-	"golang.org/x/net/context"
 )
 
+func Route(ctx context.Context) {
+	ret, ok := ctx.Value("id").(int)
+	if !ok {
+		ret = 1
+	}
+	fmt.Printf("id:%d\n", ret)
+	s, _ := ctx.Value("name").(string)
+	fmt.Printf("name:%s\n", s)
+}
+
 func main() {
-	//创建一个可取消子context,context.Background():返回一个空的Context，这个空的Context一般用于整个Context树的根节点。
-	ctx, cancel := context.WithCancel(context.Background())
-	ctxTwo, cancelTwo := context.WithCancel(context.Background())
-	go func(ctx context.Context) {
-		for {
-			select {
-			//使用select调用<-ctx.Done()判断是否要结束
-			case <-ctx.Done():
-				fmt.Println("goroutineA exit")
-				return
-			default:
-				fmt.Println("goroutineA running.")
-				time.Sleep(2 * time.Second)
-			}
-		}
-	}(ctx)
-	go func(ctx context.Context) {
-		for {
-			select {
-			//使用select调用<-ctx.Done()判断是否要结束
-			case <-ctx.Done():
-				fmt.Println("goroutineB exit")
-				return
-			default:
-				fmt.Println("goroutineB running.")
-				time.Sleep(2 * time.Second)
-			}
-		}
-	}(ctx)
-	go func(ctxTwo context.Context) {
-		for {
-			select {
-			//使用select调用<-ctx.Done()判断是否要结束
-			case <-ctxTwo.Done():
-				fmt.Println("goroutineC exit")
-				return
-			default:
-				fmt.Println("goroutineC running.")
-				time.Sleep(2 * time.Second)
-			}
-		}
-	}(ctxTwo)
-
-	time.Sleep(4 * time.Second)
-	fmt.Println("main fun exit")
-	//取消context
-	cancel()
-	cancelTwo()
-	time.Sleep(5 * time.Second)
-
+	ctx := context.WithValue(context.Background(), "id", 123)
+	ctx = context.WithValue(ctx, "name", "jerry")
+	Route(ctx)
 }
