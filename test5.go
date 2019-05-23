d18abf5f284502beb2ed4e92d88eeaa5768d6fb0
guyan0319 <guyan0319@sohu.com>
2019-05-23 23:24:30 +0800

    spider

--- test5.go
+++ test5.go
@@ -1,49 +1,29 @@
 package main
 
 import (
-	"fmt"
 	"time"
+	"fmt"
 )
 
 func main() {
-	t := time.NewTimer(time.Second * 2)
-	defer t.Stop()
+
+	ticker := time.NewTicker(2 * time.Second)
 	ch := make(chan bool)
-	for {
-		select {
-		case <-t.C:
-			fmt.Println("timer running...")
-		case stop := <-ch:
-			if stop {
-				fmt.Println("timer Stop")
-				return
+	go func(ticker *time.Ticker) {
+		defer ticker.Stop()
+		for {
+			select {
+			case <-ticker.C:
+				fmt.Println("Ticker running...")
+			case stop := <-ch:
+				if stop {
+					fmt.Println("Ticker Stop")
+					return
+				}
 			}
 		}
-		// 需要重置Reset 使 t 重新开始计时
-		t.Reset(time.Second * 2)
-	}
+	}(ticker)
 	time.Sleep(10 * time.Second)
 	ch <- true
 	close(ch)
-	//ticker := time.NewTicker(2 * time.Second)
-	//
-	//ch := make(chan bool)
-	//go func(ticker *time.Ticker) {
-	//	defer ticker.Stop()
-	//	for {
-	//		select {
-	//		case <-ticker.C:
-	//			fmt.Println("timer....")
-	//		case stop := <-ch:
-	//			if stop {
-	//				fmt.Println("Ticker2 Stop")
-	//				return
-	//			}
-	//		}
-	//	}
-	//}(ticker)
-	//
-	//time.Sleep(10 * time.Second)
-	//ch <- true
-	//close(ch)
 }
