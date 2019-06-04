9e4fd9ace762ea70af7ff787d8fd9af397491d2d
qiang <guyan0319@gmail.com>
2019-06-04 18:02:32 +0800

    param

--- test4.go
+++ test4.go
@@ -1,20 +1,13 @@
 package main
 
-import (
-	"io/ioutil"
-	"log"
-	"net/http"
-)
+import "fmt"
 
-func sayHelloHandler(w http.ResponseWriter, r *http.Request) {
-	http.Redirect(w, r, "http://www.baidu.com", http.StatusFound) //重定向
-	content := []byte("hello world")
-	err := ioutil.WriteFile("test.txt", content, 0644)
-	if err != nil {
-		panic(err)
+func multiParam(args ...string) {
+	for _, e := range args {
+		fmt.Println(e)
 	}
 }
 func main() {
-	http.HandleFunc("/", sayHelloHandler) //   设置访问路由
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	names := []string{"jerry", "herry"}
+	multiParam(names...)
 }
