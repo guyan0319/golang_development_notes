00d933637f10a9172bae2670b5616fa781389589
qiang <guyan0319@gmail.com>
2019-06-26 10:45:20 +0800

    reflect

--- test2.go
+++ test2.go
@@ -1,55 +1,13 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
+import ()
 
-func goRoutineA(a <-chan int) {
-	val := <-a
-	fmt.Println("goRoutineA received the data", val)
-}
-func goRoutineB(b chan int) {
-	val := <-b
-	fmt.Println("goRoutineB  received the data", val)
+type Result struct {
+	success int8
+	message string
+	code    int64
 }
 
-func goRoutineC(ch chan int, i int) {
-	ch <- i
-}
 func main() {
-	//ch := make(chan int, 3)
-	ch := make(chan int)
-	//fmt.Println(ch)
-	//ch := make(chan int)
-	//go goRoutineA(ch)
-	//go goRoutineB(ch)
-	//go goRoutineC(ch, 3)
-	//go goRoutineC(ch, 4)
-	//go goRoutineC(ch, 5)
-	//go goRoutineC(ch, 6)
-	//go goRoutineC(ch, 4)
 
-	//ch <- 3
-	//ch <- 4
-	//ch <- 5
-	//ch <- 6
-	//ch <- 7
-	val := <-ch
-	fmt.Println("goRoutineB  received the data", val)
-	//vv := runtime.Getchan()
-	//v := reflect.ValueOf(vv)
-	//public.Explicit(v, 0)
-	//ww := runtime.Getwaitq()
-	//w := reflect.ValueOf(ww)
-	//public.Explicit(w, 0)
-	//ch <- 3
-	//ch <- 4
-	//ch <- 5
-	//ch <- 6
-	//ch <- 7
-	//ch <- 8
-	//ch <- 9
-
-	time.Sleep(time.Second * 1)
 }
