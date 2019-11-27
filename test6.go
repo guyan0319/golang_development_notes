decb49406e42a9dcc11b82a33845b766a22a4fb1
qiang <guyan0319@gmail.com>
2019-11-27 17:20:24 +0800

    mod

--- test6.go
+++ test6.go
@@ -1,129 +1,17 @@
 package main
 
 import (
-	"bytes"
-	"fmt"
-	"github.com/PuerkitoBio/goquery"
-	"github.com/tebeka/selenium"
-	"github.com/tebeka/selenium/chrome"
-	"time"
+	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func main() {
-
-	const (
-		seleniumPath = `C:\Program Files (x86)\chromedriver\chromedriver.exe`
-		port         = 9515
-	)
-
-	//如果seleniumServer没有启动，就启动一个seleniumServer所需要的参数，可以为空，示例请参见https://github.com/tebeka/selenium/blob/master/example_test.go
-	opts := []selenium.ServiceOption{}
-	//opts := []selenium.ServiceOption{
-	//    selenium.StartFrameBuffer(),           // Start an X frame buffer for the browser to run in.
-	//    selenium.GeckoDriver(geckoDriverPath), // Specify the path to GeckoDriver in order to use Firefox.
-	//}
-
-	//selenium.SetDebug(true)
-	service, err := selenium.NewChromeDriverService(seleniumPath, port, opts...)
-	if nil != err {
-		fmt.Println("start a chromedriver service falid", err.Error())
-		return
-	}
-	//注意这里，server关闭之后，chrome窗口也会关闭
-	defer service.Stop()
-
-	//链接本地的浏览器 chrome
-	caps := selenium.Capabilities{
-		"browserName": "chrome",
-	}
-
-	//禁止图片加载，加快渲染速度
-	imagCaps := map[string]interface{}{
-		"profile.managed_default_content_settings.images": 2,
-	}
-	chromeCaps := chrome.Capabilities{
-		Prefs: imagCaps,
-		Path:  "",
-		Args: []string{
-			//"--headless", // 设置Chrome无头模式，在linux下运行，需要设置这个参数，否则会报错
-			//"--no-sandbox",
-			"--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36", // 模拟user-agent，防反爬
-		},
-	}
-	//以上是设置浏览器参数
-	caps.AddChrome(chromeCaps)
-
-	// 调起chrome浏览器
-	w_b1, err := selenium.NewRemote(caps, fmt.Sprintf("http://localhost:%d/wd/hub", port))
-	if err != nil {
-		fmt.Println("connect to the webDriver faild", err.Error())
-		return
-	}
-	//关闭一个webDriver会对应关闭一个chrome窗口
-	//但是不会导致seleniumServer关闭
-	defer w_b1.Quit()
-	err = w_b1.Get("https://blog.csdn.net/guyan0319")
-	if err != nil {
-		fmt.Println("get page faild", err.Error())
-		return
-	}
-	//判断加载完成
-	jsRt, err := w_b1.ExecuteScript("return document.readyState", nil)
-	if err != nil {
-		fmt.Println("exe js err", err)
-	}
-	fmt.Println("jsRt", jsRt)
-	if jsRt != "complete" {
-		fmt.Println("网页加载未完成")
-		return
-	}
-	//获取网站内容
-	var frameHtml string
-	time.Sleep(1 * time.Second)
-	frameHtml, err = w_b1.PageSource()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	//解析html 文件
-	var doc *goquery.Document
-	doc, err = goquery.NewDocumentFromReader(bytes.NewReader([]byte(frameHtml)))
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	doc.Find(".article-item-box").Each(func(index int, item *goquery.Selection) {
-		title := item.Text()
-		linkTag := item.Find("a")
-		link, _ := linkTag.Attr("href")
-		fmt.Printf("Post #%d: %s - %s\n", index, title, link)
+	r := gin.Default()
+	//访问实例 /login?param1=a&param2=b&ids[1]=f&ids[2]=h
+	r.GET("/login", func(c *gin.Context) {
+		param1 := c.DefaultQuery("param1", "c") //设置初始值
+		param2 := c.Query("param2")
+		c.String(http.StatusOK, "Hello world "+param1+"_"+param2)
 	})
-
-	//// 重新调起chrome浏览器
-	//w_b2, err := selenium.NewRemote(caps, fmt.Sprintf("http://localhost:%d/wd/hub", port))
-	//if err != nil {
-	//	fmt.Println("connect to the webDriver faild", err.Error())
-	//	return
-	//}
-	//defer w_b2.Close()
-	////打开一个网页
-	//err = w_b2.Get("https://www.toutiao.com/")
-	//if err != nil {
-	//	fmt.Println("get page faild", err.Error())
-	//	return
-	//}
-	////打开一个网页
-	//err = w_b2.Get("https://www.baidu.com/")
-	//if err != nil {
-	//	fmt.Println("get page faild", err.Error())
-	//	return
-	//}
-	//w_b就是当前页面的对象，通过该对象可以操作当前页面了
-	//........
-	time.Sleep(5 * time.Minute)
-	return
-}
-func openPage() {
-
+	r.Run(":8282")
 }
